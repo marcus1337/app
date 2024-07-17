42ace7e0920133ffff1977fade80e56282500bdc
marcus1337 <blurgas@hotmail.com>
2024-07-17 14:04:47 +0200

    wasm example

---
+++ example/README.md
@@ -0,0 +1,7 @@
+## WASM Example
+
+This example shows how to create a web app. To quickly run the example use the Dockerfile; <br>
+``sudo docker build -t appf .`` <br>
+``sudo docker run -it -p 8000:8000 appf`` <br>
+
+Open any modern browser and go to `localhost:8000/`. Press `F12` for console logs or other debug info.
--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,35 @@
-#include "app.h"
-#include <emscripten.h>
+#ifdef EMSCRIPTEN
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#include <emscripten/fetch.h>
+#include <emscripten/bind.h>
+#endif
 
-extern "C" {
-    EMSCRIPTEN_KEEPALIVE
-    int main() {
-        //std::cout << "Hello World WASM example!\n";
-        return 0;
-    }
+#include "app.h"
+
+void resizeWindow(int width, int height)
+{
+    std::cout << "Resizing: " << width << "," << height << "\n";
+}
+
+uint64_t counter = 0;
+
+void step()
+{
+    std::cout << "step: " << counter++ << "\n";
+}
+
+#ifdef EMSCRIPTEN
+EMSCRIPTEN_BINDINGS(resize_module)
+{
+    emscripten::function("resizeWindow", &resizeWindow);
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+#ifdef EMSCRIPTEN
+    emscripten_set_main_loop(step, 0, 1);
+#endif
+    return 0;
 }
