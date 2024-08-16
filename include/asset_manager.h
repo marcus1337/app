f6b15f7b7f5417745a505999308463c1db1eec06
marcus1337 <blurgas@hotmail.com>
2024-08-16 21:20:55 +0200

    refactor

--- include/asset_manager.h
+++ include/asset_manager.h
@@ -2,42 +2,10 @@
 #pragma once
 #include "util/pch.h"
 #include "util/error.h"
+#include "util/file.h"
 
 namespace appf
 {
-
-    struct File
-    {
-        File(const std::string &filename) : name(filename)
-        {
-        }
-        File(const std::vector<std::string> &upperDirs, const std::string &filename) : name(filename), upperDirs(upperDirs)
-        {
-        }
-
-        bool hasMatchingUpperDirs(const std::filesystem::path &path) const
-        {
-            int counter = upperDirs.size();
-            auto tmpPath = path;
-            while (counter-- > 0)
-            {
-                if (!tmpPath.has_parent_path() || tmpPath.parent_path().filename() != upperDirs[counter])
-                {
-                    return false;
-                }
-                tmpPath = tmpPath.parent_path();
-            }
-            return true;
-        }
-
-        bool isMatch(const std::filesystem::path &path) const
-        {
-            return hasMatchingUpperDirs(path) && path.filename() == name;
-        }
-
-        std::string name;
-        std::vector<std::string> upperDirs; // Sorted furthest away to most immediate.
-    };
 
     class AssetManager
     {
