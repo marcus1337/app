85c629af0794bdb919730a690c787a73be0727fe
marcus1337 <blurgas@hotmail.com>
2024-08-16 14:50:59 +0200

    app context

--- include/app.h
+++ include/app.h
@@ -10,44 +10,13 @@ namespace appf
     class App
     {
     public:
-        App()
-        {
-            if (initSDL())
-            {
-                AssetManager::init();
-                initialized = true;
-            }
-        }
-
-        ~App()
-        {
-            AssetManager::shutDown();
-            if (ini::sdlInitialized)
-            {
-                ini::quitSDL();
-            }
-        }
-
-        bool isIntialized() const
-        {
-            return initialized;
-        }
+        App();
+        ~App();
+        bool isIntialized() const;
 
     private:
         bool initialized = false;
-
-        bool initSDL() const
-        {
-            if (!ini::sdlInitialized)
-            {
-                if (auto error = ini::initSDL(); error.has_value())
-                {
-                    std::cerr << error.value().getMessage() << "\n";
-                    return false;
-                }
-            }
-            return ini::sdlInitialized;
-        }
+        bool initSDL() const;
     };
 
 }
--- src/app.cpp
+++ src/app.cpp
@@ -1,2 +1,39 @@
 #include "app.h"
 
+using namespace appf;
+
+App::App()
+{
+    if (initSDL())
+    {
+        AssetManager::init();
+        initialized = true;
+    }
+}
+
+App::~App()
+{
+    AssetManager::shutDown();
+    if (ini::sdlInitialized)
+    {
+        ini::quitSDL();
+    }
+}
+
+bool App::isIntialized() const
+{
+    return initialized;
+}
+
+bool App::initSDL() const
+{
+    if (!ini::sdlInitialized)
+    {
+        if (auto error = ini::initSDL(); error.has_value())
+        {
+            std::cerr << error.value().getMessage() << "\n";
+            return false;
+        }
+    }
+    return ini::sdlInitialized;
+}
